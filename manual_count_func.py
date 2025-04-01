b341290ef4e8922921f903fb70d708f5e65309f5
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 16:43:50 +0800

    used count start init to follow every find for word desired, then breaks loop func if it can't find any word after counting all the words, then print accumulated count value

--- manual_count_func.py
+++ manual_count_func.py
@@ -1,7 +1,20 @@
 # Input sentence
+user_input = str(input("Enter a sentence: "))
 
 # Input desired word to count
+user_word = str(input("Enter desired word to count in the sentence provided: "))
 
 # Count words in sentence
+count = 0
+start = 0
+
+while True:
+    start = user_input.find(user_word, start)
+    if start == -1:
+        break
+
+    count += 1
+    start += len(user_word)
 
 # Print word count
+print(count)
