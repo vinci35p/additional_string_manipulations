5506fe84569534147d69d2ebd140bbef005316a4
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 18:13:06 +0800

    referenced from last code manual index func, just added -1, -1 in parameters to set where it'll start to find the desired char

--- manual_r_index.py
+++ manual_r_index.py
@@ -1,7 +1,15 @@
 # Input sentence
+user_sentence = str(input("Enter a sentence: "))
 
 # Input desired word to be indexed
+user_word = str(input("Enter a character to find: "))
 
 # Find desired word starting from the end, then determine its placement, then print its placement
+for i in range(len(user_sentence) - len(user_word), -1, -1):
+    if user_sentence[i: i + len(user_word)] == user_word:
+        print(f"The first location of {user_word} in the sentence, is at index: {i}")
+        break
 
 # Else, if not found, print not found in sentence
+    else:
+        print(f"The indicated character was not found in the sentence.")
