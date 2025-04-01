3bdc08ec03178adcedf4d90087a49aff80322caa
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 16:01:05 +0800

    used two input func, and format func to manipulate string into adding spaces after char

--- manual_r_just.py
+++ manual_r_just.py
@@ -1,7 +1,11 @@
 # Input any characters
+user_input = str(input("Enter any string characters: "))
 
 # Input how many characters desired
+desired_char = str(input(f"Enter desired whole characters width (current width count : {len(user_input)}): "))
 
 # Justify string based on number of characters desired
+just_char = "{:<{}}".format(user_input, desired_char)
 
 # Print the justified string
+print(just_char)
