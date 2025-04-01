1fd80bdd9d2db91541ad964e695e0eba20c86278
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 13:32:54 +0800

    added str func next to user_num as it is not working when not converted to string

--- manual_remove_suffix.py
+++ manual_remove_suffix.py
@@ -5,7 +5,7 @@ user_input = str(input("Enter any characters that has several numbers at the end
 user_num = int(input("Enter the numbers at the end you want to remove: "))
 
 # Remove desired suffix
-new_input = user_input.rstrip(user_num)
+new_input = user_input.rstrip(str(user_num))
 
 # Print new string
 print(new_input)
