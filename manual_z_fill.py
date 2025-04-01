269089adb56aec5c5de5307279f4817395de6a0e
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 16:13:22 +0800

    used rjust but it counts characters with string limiting full count of zeros

--- manual_z_fill.py
+++ manual_z_fill.py
@@ -1,7 +1,11 @@
 # Input any characters
+user_input = str(input("Enter any string characters: "))
 
 # Input how many zeros to add at the beginning
+zero_num = int(input("Enter how many zeros to add: "))
 
 # Add zeros at the beginning of the string
+modified_char = user_input.rjust(zero_num, '0')
 
 # Print modified string
+print(modified_char)
