aa2a4422b6a1ecac7ce75fb9795394fd03af7b2d
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 13:15:22 +0800

    used strip function, but if there is spaces at left it also deletes it

--- manual_r_strip.py
+++ manual_r_strip.py
@@ -1,5 +1,8 @@
 # Input full name with spaces after
+user_name = str(input("Enter your full name with several spaces after: "))
 
 # Remove spaces after name
+spaceless_name = user_name.rstrip()
 
 # Print spaceless name
+print(spaceless_name)
