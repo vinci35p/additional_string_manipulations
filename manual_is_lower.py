c6386925141625a31686f574184f5c90fb23f169
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 13:42:10 +0800

    referenced to code is upper just changed .upper func to .lower to check if lower case

--- manual_is_lower.py
+++ manual_is_lower.py
@@ -1,5 +1,10 @@
 # Input any characters
+user_input = str(input("Enter any characters: "))
 
 # Check if characters inputted is in lower case, then print if it is
+if user_input == user_input.swapcase().lower():
+    print("The string inputted is all in lower case.")
 
 # If it is not, print not in lower case
+else:
+    print("All or some string inputted is not in lower case.")
