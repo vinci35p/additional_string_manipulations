ea8e6ab63825a42a25145edfcde64018cd7c6f14
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 17:09:09 +0800

    created pseudocode for manual_index_func

---
+++ manual_index_func.py
@@ -0,0 +1,9 @@
+# Input several characters
+
+# Input the character to be indexed
+
+# Find the character in the list
+
+# Print the placement of character in list
+
+# If not found, print not found in list
