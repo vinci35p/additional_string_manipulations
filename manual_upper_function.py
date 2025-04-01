7d399d486e7dd69997118fdf315396fba5c962bf
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 13:52:28 +0800

    used lower to lower entered string, then swapcase to swtich to upper case

--- manual_upper_function.py
+++ manual_upper_function.py
@@ -1,5 +1,10 @@
 # Input any characters
+user_input = str(input("Enter any string characters: "))
 
 # Convert inputted characters into upper case
+lower_case_input = user_input.lower()
+
+upper_case = lower_case_input.swapcase()
 
 # Print converted string
+print(upper_case)
