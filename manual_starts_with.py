1da13a85f37de6038474e86c6fde16d8ee917c59
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 14:28:23 +0800

    used find func instead, if it equals to zero it does start with desired value

--- manual_starts_with.py
+++ manual_starts_with.py
@@ -5,9 +5,9 @@ user_sentence = str(input("Enter a sentence: "))
 word_value = str(input("Enter desired value to check if it the sentence starts with it: "))
 
 # Print true value if it starts with desired word value
-if user_sentence[len(word_value):] == word_value:
-    print("The sentence starts with the desired value!")
+if user_sentence.find(word_value) == 0:
+    print(f"The sentence does start with {word_value}.")
 
 # Print false value if it doesn't
 else:
-    print("The sentence doesn't start with the desired value.")
+    print(f"The sentence doesn't start with {word_value}.")
