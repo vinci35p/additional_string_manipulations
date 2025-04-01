5a95be9a9c4f54846bc359169b69f642029d8878
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 13:31:58 +0800

    used rstrip indicating parameters based on user desire

--- manual_remove_suffix.py
+++ manual_remove_suffix.py
@@ -1,7 +1,11 @@
 # Input any characters
+user_input = str(input("Enter any characters that has several numbers at the end: "))
 
 # Indicate suffix to remove
+user_num = int(input("Enter the numbers at the end you want to remove: "))
 
 # Remove desired suffix
+new_input = user_input.rstrip(user_num)
 
 # Print new string
+print(new_input)
