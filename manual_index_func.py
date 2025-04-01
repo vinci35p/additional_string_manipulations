79979bcb8b851a41f3bb07215bf544e622ae5136
Vincent35p <vinsoyt4321@gmail.com>
2025-04-01 17:48:15 +0800

    used loop to check if it matches starting at current index, then len func to count string to indicate stopping point, and used calculations to see if it matched then it is the first occurrence

--- manual_index_func.py
+++ manual_index_func.py
@@ -1,9 +1,15 @@
 # Input sentence
+user_sentence = str(input("Enter a sentence: "))
 
 # Input desired word to be indexed
+user_word = str(input("Enter a character to find: "))
 
-# Find desired in provided sentence, then determine its placement
+# Find desired in provided sentence, then determine its placement, then print its placement in the sentence
+for i in range(len(user_sentence) - len(user_word) + 1):
+    if user_sentence[i: i + len(user_word)] == user_word:
+        print(f"The first location of {user_word} in the sentence, is at index: {i}")
+        break
 
-# Print if it is found and its placement in the sentence
-
-# If not found, print not found in list
+# Else, if not found, print not found in list
+    else:
+        print(f"The indicated character was not found in the sentence.")
